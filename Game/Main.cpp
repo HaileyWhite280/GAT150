1fe41fe9e7655f9e14756a994604c26cef623881
Meliemau <80057246+Meliemau@users.noreply.github.com>
2021-08-22 21:55:59 -0600

    Semi finished JSON
    Need to finish Get for Vector2 and Color, having issues with "expression must have a modifiable lvalue" on line 93 and 113 of Json.cpp

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "Core/Json.h"
 
 //check out GameArt.org
 //TODO Do Text and Game Port and JSON
@@ -9,6 +10,38 @@ int main(int, char**)
 {
 	Game game;
 	game.Initialize();
+
+	rapidjson::Document document;
+	bool success = nc::json::Load("json.txt", document);
+	assert(success);
+
+	std::string str;
+	nc::json::Get(document, "string", str);
+	std::cout << str << std::endl;
+
+	bool b;
+	nc::json::Get(document, "boolean", b);
+	std::cout << b << std::endl;
+
+	int i1;
+	nc::json::Get(document, "integer1", i1);
+	std::cout << i1 << std::endl;
+
+	int i2;
+	nc::json::Get(document, "integer2", i2);
+	std::cout << i2 << std::endl;
+
+	float f;
+	nc::json::Get(document, "float", f);
+	std::cout << f << std::endl;
+
+	nc::Vector2 v2;
+	nc::json::Get(document, "vector2", v2);
+	std::cout << v2 << std::endl;
+
+	nc::Color color;
+	nc::json::Get(document, "color", color);
+	std::cout << color << std::endl;
 
 	bool quit = false;
 	SDL_Event event;
